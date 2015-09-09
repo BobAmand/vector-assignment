9bd3f58be9149c7290f0e54fa9245bf1d2d65381
Bob Amand <biotmg@nc.rr.com>
2015-09-09 17:30:15 -0400

    draft 09-Sep-2015 5:30pm

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,16 +1,33 @@
 class ShapeException(Exception):
     pass
 
+def shape(vector):
+    shape = len(vector)
+    return (shape, )
 
-v = [1, 3, 0]
-w = [0, 2, 4]
+# v = [1, 3, 0]
+# w = [0, 2, 4]
 
 def vector_add(v, w):
     '''
     Adds two lists by aligned indexes:
     old way first, list comprehensive second.
     '''
-    newmat = []
-    for r in range(len(v)):
-            newmat.append(v[r] + w[r])
+    # newmat = []
+    # for r in range(len(v)):
+    #     newmat.append(v[r] + w[r])
+    # return newmat
+    newmat = [(v[r]+ w[r]) for r in range(len(v))]
+    return newmat
+
+def vector_sub(v, w):
+    '''
+    Subtracts two lists by aligned indexes:
+    old way first, list comprehensive second.
+    '''
+    # newmat = []
+    # for r in range(len(v)):
+    #     newmat.append(v[r] - w[r])
+    # return newmat
+    newmat = [(v[r] - w[r]) for r in range(len(v))]
     return newmat
