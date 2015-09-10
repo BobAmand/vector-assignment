5542ffc04d981279f91ea3766ee0b4e583eabb89
Bob Amand <biotmg@nc.rr.com>
2015-09-09 23:16:03 -0400

    draft 09-Sep-2015 11:15 pm

--- linear_algebra.py
+++ linear_algebra.py
@@ -5,8 +5,8 @@ def shape(vector):
     shape = len(vector)
     return (shape, )
 
-# v = [1, 3, 0]
-# w = [0, 2, 4]
+def is_equal(x, y, tolerance=0.001):
+    return abs(x-y) <= tolerance
 
 def vector_add(v, w):
     '''
@@ -32,6 +32,16 @@ def vector_sub(v, w):
     newmat = [(v[r] - w[r]) for r in range(len(v))]
     return newmat
 
+def vector_sum():
+    '''
+    No input! But needs all vectors of same length.
+    Conditional on all vectors are the same length.
+
+    '''
+    # need to ripple through the parings of all vectors and compare length:
+    #
+
+
 def dot(v, w):
     new_sum = 0
     newmat = [(v[r] * w[r]) for r in range(len(v))]
@@ -41,3 +51,36 @@ def dot(v, w):
     '''
     TODO: integrate the sum of the newmat list into the list comprehensives
     '''
+
+def vector_multiply(vector, constant):
+    '''
+    No input in the test function.  Do not understand the error/how test is testing.
+    '''
+    vector_prod = [(vector[r] * constant) for r in range(len(vector))]
+    return vector_prod
+
+def vector_mean(vect_a, vect_b, vect_c):
+    '''
+    No input in the test function. Started with old school coding.
+    '''
+    sum_list =[]
+    mean_list =[]
+    denom = len(vect_a)
+    for a in range(len(vect_a)):
+        for b in range(len(vect_b)):
+            for c in range(len(vect_c)):
+                sum_list.append(vect_a[a] + vect_b[b] + vect_c[c])
+    for i in range(len(sum_list)):
+        mean_list.append(sum_list[i]/denom)
+    return mean_list
+    '''
+    TODO translate into list conditional.
+    '''
+    
+
+
+
+
+
+#
+# def magnitude():
--- test_linear_algebra.py
+++ test_linear_algebra.py
@@ -1,93 +1,84 @@
 from linear_algebra import *
 from nose.tools import raises
 
-
 # def is_equal(x, y, tolerance=0.001):
 #     """Helper function to compare floats, which are often not quite equal
 #     even when they should be."""
 #     return abs(x - y) <= tolerance
-
-
-m = [3, 4]
-n = [5, 0]
-
-v = [1, 3, 0]
-w = [0, 2, 4]
-u = [1, 1, 1]
-y = [10, 20, 30]
-z = [0, 0, 0]
-
-
-def test_shape_vectors():
-    """shape should take a vector or matrix and return a tuple with the
-    number of rows (for a vector) or the number of rows and columns
-    (for a matrix.)"""
-    assert shape(m) == (2,)
-    assert shape(v) == (3,)
-    assert shape([1]) == (1,)
-
-
-def test_vector_add():
-    """
-    [a b]  + [c d]  = [a+c b+d]
-
-    Matrix + Matrix = Matrix
-    """
-    assert vector_add(v, w) == [1, 5, 4]
-    assert vector_add(u, y) == [11, 21, 31]
-    assert vector_add(u, z) == u
-
 #
-def test_vector_add_is_commutative():
-    assert vector_add(w, y) == vector_add(y, w)
-
-
+# m = [3, 4]
+# n = [5, 0]
+#
+# v = [1, 3, 0]
+# w = [0, 2, 4]
+# u = [1, 1, 1]
+# y = [10, 20, 30]
+# z = [0, 0, 0]
 #
 #
-# @raises(ShapeException)
-# def test_vector_sub_checks_shapes():
-#     """Shape rule: the vectors must be the same size."""
-#     vector_sub(m, v)
+# def test_shape_vectors():
+#     """shape should take a vector or matrix and return a tuple with the
+#     number of rows (for a vector) or the number of rows and columns
+#     (for a matrix.)"""
+#     assert shape(m) == (2,)
+#     assert shape(v) == (3,)
+#     assert shape([1]) == (1,)
 #
 #
-# def test_vector_sum():
-#     """vector_sum can take any number of vectors and add them together."""
-#     assert vector_sum(v, w, u, y, z) == [12, 26, 35]
-#
-#
-# @raises(ShapeException)
-# def test_vector_sum_checks_shapes():
-#     """Shape rule: the vectors must be the same size."""
-#     vector_sum(v, w, m, y)
-#
-#
-def test_dot():
-    """
-    dot([a b], [c d])   = a * c + b * d
-
-    dot(Vector, Vector) = Scalar
-    """
-    assert dot(w, y) == 160
-    assert dot(m, n) == 15
-    assert dot(u, z) == 0
-
-
-# @raises(ShapeException)
-# def test_dot_checks_shapes():
-#     """Shape rule: the vectors must be the same size."""
-#     dot(v, m)
-#
-#
-# def test_vector_multiply():
+# def test_vector_add():
 #     """
-#     [a b]  *  Z     = [a*Z b*Z]
+#     [a b]  + [c d]  = [a+c b+d]
 #
-#     Vector * Scalar = Vector
+#     Matrix + Matrix = Matrix
 #     """
-#     assert vector_multiply(v, 0.5) == [0.5, 1.5, 0]
-#     assert vector_multiply(m, 2) == [6, 8]
+#     assert vector_add(v, w) == [1, 5, 4]
+#     assert vector_add(u, y) == [11, 21, 31]
+#     assert vector_add(u, z) == u
 #
 #
+# def test_vector_add_is_commutative():
+#     assert vector_add(w, y) == vector_add(y, w)
+
+@raises(ShapeException)
+def test_vector_sub_checks_shapes():
+    """Shape rule: the vectors must be the same size."""
+    vector_sub(m, v)
+
+def test_vector_sum():
+    """vector_sum can take any number of vectors and add them together."""
+    assert vector_sum(v, w, u, y, z) == [12, 26, 35]
+
+@raises(ShapeException)
+def test_vector_sum_checks_shapes():
+    """Shape rule: the vectors must be the same size."""
+    vector_sum(v, w, m, y)
+
+# def test_dot():
+#     """
+#     dot([a b], [c d])   = a * c + b * d
+#
+#     dot(Vector, Vector) = Scalar
+#     """
+#     assert dot(w, y) == 160
+#     assert dot(m, n) == 15
+#     assert dot(u, z) == 0
+
+@raises(ShapeException)
+def test_dot_checks_shapes():
+    """Shape rule: the vectors must be the same size."""
+    dot(v, m)
+
+
+def test_vector_multiply():
+    """
+    [a b]  *  Z     = [a*Z b*Z]
+
+    Vector * Scalar = Vector
+    """
+    assert vector_multiply(v, 0.5) == [0.5, 1.5, 0]
+    assert vector_multiply(m, 2) == [6, 8]
+
+
 # def test_vector_mean():
 #     """
 #     mean([a b], [c d]) = [mean(a, c) mean(b, d)]
