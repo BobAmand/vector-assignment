e3907efc73ec1147d38e58b51c9a82cc21b86a91
Bob Amand <biotmg@nc.rr.com>
2016-01-23 13:55:46 -0500

    First 11 tests pass

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,5 +1,9 @@
+from math import sqrt
+
+
 class ShapeException(Exception):
     pass
+bad_shape = "The vectors must be the same size."
 
 
 def shape(vec):
@@ -26,6 +30,8 @@ def vector_add(v, w):
 
 
 def vector_sub(v, w):
+    if shape(v) != shape(w):
+        raise ShapeException("Shape rule: the vectors must be the same size.")
     '''
     Subtracts two lists by aligned indexes:
     old way first, list comprehensive second.
@@ -39,10 +45,18 @@ def vector_sub(v, w):
 
 
 def vector_sum(*args):
+    lst = [len(x) for x in args]
+    if any(x != lst[0] for x in lst):
+        raise ShapeException("Shape rule: the vectors must be the same size.")
+    else:
+        return [sum(x) for x in zip(*args)]
+'''
+my initial:
     ret = [0] * len(args[0])  # initialize with series of zeros
     for x in args:
         ret = vector_add(ret, x)
     return ret
+'''
 '''
 alternate:
     return [sum(x) for x in zip(*args)]
@@ -74,16 +88,17 @@ alternate:
 # return sum_list
 '''
 #    TODO translate into list conditional.
-'''
+
 
 def dot(v, w):
-    newmat = sum([(v[r] * w[r]) for r in range(len(v))])
-    return newmat
-'''
-#    Applied the sum() to the whole conditional after in-class review
+    if len(v) != len(w):
+        raise ShapeException("ShapeException: lists are not the same size.")
+    else:
+        newmat = [(v[r] * w[r]) for r in range(len(v))]  # list of products
+        return sum(newmat)    # sums list contents
 
 
-def vector_multiply(vec, sca):
+def vector_multiply(vec, sca):   # multiply by a constant (scalar)
     return [sca * x for x in vec]
 '''
     Assume vector, constant are being passed in.
@@ -93,12 +108,19 @@ def vector_multiply(vec, sca):
     return vector_prod
 '''
 
+
 def vector_mean(*args):
-    num = len(args)         # how many tuples in the args list
-    vector = vector_sum(*args) # unpacks the args.
-    return vector_multiply(vector/1)
+    return([v/len([x for x in args]) for v in vector_sum(*[x for x in args])])
+
+    # num = len(args)         # how many tuples in the args list
+    # vector = vector_sum(*args)  # unpacks the args.
+    # return vector/num
 
     '''
+    Alternative:
+    return([v/len([x for x in args]) for v in vector_sum(*[x for x in args])])
+     - glenn hurley solution!
+
     Started with old school coding.
     Assumed (vect_a, vect_b, vect_c) are being passed in.
     My initial effort:
@@ -115,10 +137,13 @@ def vector_mean(*args):
     return mean_list
     '''
 
+
 def magnitude(vec):
-    return math.sqrt(dot(vec, vec))
+    return sqrt(sum([x**2 for x in vec]))
     '''
     My initial effort:
-        magnitude = [(vect_a[i]**2 + vect_b[i]**2)**0.5 for i in range(len(vect_a))]
-        return magnitude
+    return math.sqrt(dot(vec, vec))
+
+    magnitude = [(vect_a[i]**2 + vect_b[i]**2)**0.5 for i in range(len(vect_a))]
+    return magnitude
     '''
